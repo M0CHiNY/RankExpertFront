d1dd1174bb0da73390db40ea2efafb8293d2e449
Volodymyr <volodimrmihnyk@gmail.com>
2023-11-24 14:53:33 +0200

    add new blocks for service

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,10 @@ import '../scss/app.scss';
 /* HTML PARTS INCLUDE */
 import './inc-html.js';
 
+/* import jolty library */
+import './libs/joltyComponents/jolty-main.js';
+
 /* init swiper slider */
 import './swiper.js';
 
-/* import jolty library */
-import './libs/joltyComponents/jolty-main.js';
 
