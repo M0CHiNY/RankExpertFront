d0afc77b1063da5b84dc2aab91b072be4f9d6bf2
Volodymyr <volodimrmihnyk@gmail.com>
2023-12-06 18:57:28 +0200

    add new block marketing

--- src/js/script.js
+++ src/js/script.js
@@ -82,39 +82,6 @@ document.addEventListener("DOMContentLoaded", function () {
   
   //scroll bar
 
-  // const line = document.querySelector(".service-steps__bar");
-  // const sectioWrap = document.querySelector(".service-steps");
-  // // const sectioWrapItems = sectioWrap.querySelectorAll(".service-steps__item");
-
-  // const progressAnimation = () => {
-  //   let scrollTop = window.scrollY;
-  //   // let windowCentr = window.innerHeight / 2 + scrollTop;
-  //   let topWrap = sectioWrap.offsetTop;
-  //   let heightWrap = sectioWrap.offsetHeight;
-
-
-  //   if (scrollTop >= topWrap) {
-  //     let percentageProgress = Math.floor((scrollTop / heightWrap) * 100);
-  //     line.style.height = `${percentageProgress}%`;
-  //   } else {
-  //     line.style.height = 0;
-  //   }
-  //   // if (scrollTop >= topWrap) {
-  //   //   sectioWrapItems.forEach((el, index) => {
-  //   //     if (index % 2 !== 0) {
-  //   //       let scrolOfset = el.offsetTop;
-  //   //         let percentageProgress = Math.floor(
-  //   //           (scrollTop / (windowCentr - (heightWrap - scrolOfset)) * 100));
-  //   //         console.log(heightWrap);
-  //   //         line.style.height = `${percentageProgress}%`;          
-  //   //         console.log(el.offsetTop);
-  //   //     }
-  //   //   });
-  //   // }
-  // };
-
-  // progressAnimation();
-  // window.addEventListener("scroll", progressAnimation);
 
 })
 
